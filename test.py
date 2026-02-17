3ad23d72f5bcd3527fa153d304217307a6132d54
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-17 20:51:43 +0200

    now we start the server after creating the endpoints

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import asyncio
 
 
 server = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
-asyncio.run(server.serve_forever())
 
 server.create_custom_endpoint(endpoint_method="GET", endpoint_url="http://127.0.0.1:8000/return100", endpoint_function="print(100)")
+
+asyncio.run(server.serve_forever())
