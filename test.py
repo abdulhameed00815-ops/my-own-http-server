09dcfddf5bc98e9ac8ce4e08ea68c361a56e35e0
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-18 23:44:00 +0200

    now the client can create endpoints

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from server import PicoTCPServer
+from server import PicoTCPServer, PicoHTTPRequestHandler
 import asyncio
 
 
 server = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
 
-server.create_custom_endpoint(endpoint_method="GET", endpoint_url="http://127.0.0.1:8000/return100", endpoint_function="print(100)")
+server.create_custom_endpoint(
+endpoint_method="GET", 
+endpoint_url="http://127.0.0.1:8000/return100", 
+endpoint_function="""
+function():
+    return 100
+""")
 
 asyncio.run(server.serve_forever())
