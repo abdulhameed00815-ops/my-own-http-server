b8a443a74ebaf9a6fe2198aa1c612bb26031bc80
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-19 13:03:05 +0200

    now the endpoint creation replicates fastapi (not working yet)

--- test.py
+++ test.py
@@ -4,12 +4,9 @@ import asyncio
 
 server = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
 
-server.create_custom_endpoint(
-endpoint_method="GET", 
-endpoint_url="http://127.0.0.1:8000/return100", 
-endpoint_function="""
-function():
-    return 100
-""")
+@server.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100", endpoint_function)
+def function():
+    print("100")
+
 
 asyncio.run(server.serve_forever())
