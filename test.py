672adff9c561bbe8bef089a3874b1545b488d695
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-19 17:28:21 +0200

    decorator still not working as should

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from server import PicoTCPServer, PicoHTTPRequestHandler
 import asyncio
 
 
-server = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
+app = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
 
-@server.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100", endpoint_function)
+@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100")
 def function():
     print("100")
 
 
-asyncio.run(server.serve_forever())
+asyncio.run(app.serve_forever())
