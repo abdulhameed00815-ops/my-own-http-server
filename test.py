3a964b483d00a69af77aca8c84e15877fe452cfe
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-21 23:42:09 +0200

    endpoints close to work

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-from server import PicoTCPServer, PicoHTTPRequestHandler
+import server
 import asyncio
 
 
-app = PicoTCPServer(("127.0.0.1", 8000), PicoHTTPRequestHandler)
+routes = {}
 
-@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100")
+server_instance = server.PicoTCPServer(("127.0.0.1", 8000), server.PicoHTTPRequestHandler)
+
+app = server.RoutesHandler()
+
+
+@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100", routes=routes)
 def function():
     print("100")
 
 
-asyncio.run(app.serve_forever())
+print(routes)
+
+
+asyncio.run(server_instance.serve_forever())
