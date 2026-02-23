6f97f3ffe04ce66701c306ea79ca34f84172afb7
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-23 23:49:52 +0200

    testing with more complex functions

--- test.py
+++ test.py
@@ -2,16 +2,15 @@ from server import PicoHTTPRequestHandler, PicoTCPServer, RoutesHandler
 import asyncio
 
 
-routes = {}
 app = RoutesHandler()
 
 
-@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="return100", routes=routes)
-def function():
-    print("100")
+@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="multiply/{x}/{y}")
+def function(x: int, y: int):
+    return x * y
 
 
-print(routes)
+routes = app.routes
 
 server_instance = PicoTCPServer(routes=routes, request_handler=PicoHTTPRequestHandler, socket_address=("127.0.0.1", 8000))
 
