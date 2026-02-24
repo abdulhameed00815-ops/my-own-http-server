0560abdfecbce67d924f7dc716490ce21808f8f7
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-24 17:48:00 +0200

    tested multiple endpoints (now not working because server code is not yet complete)

--- test.py
+++ test.py
@@ -10,6 +10,11 @@ def function(x: int, y: int):
     return x * y
 
 
+@app.create_custom_endpoint(endpoint_method="GET", endpoint_url="mix/{x}/{y}")
+def function(x: str, y: str):
+    return x + y
+
+
 routes = app.routes
 
 server_instance = PicoTCPServer(routes=routes, request_handler=PicoHTTPRequestHandler, socket_address=("127.0.0.1", 8000))
